d73b30a42e9548febf16c8c0c457ceeb32c461d6
zhongzilu <zhongzilu520@163.com>
2016-05-27 15:55:28 +0800

    修改为适配不同屏幕分别率
    1.删除了一些没用的(no used)文件和代码
    2.将原来的硬编码(hard code)部分修改为根据屏幕尺寸动态计算

--- app/src/main/java/com/zhongzilu/tuner/MainActivity.java
+++ app/src/main/java/com/zhongzilu/tuner/MainActivity.java
@@ -1,22 +1,38 @@
 package com.zhongzilu.tuner;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
-/**
- * Created by zhongzilu on 16-5-18.
- */
 public class MainActivity extends AppCompatActivity {
 
-    DialView view;
+    private CheckBox mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        view = new DialView(this, null);
-        setContentView(view);
+        setContentView(R.layout.activity_main);
 
+        mStartButton = (CheckBox) findViewById(R.id.startButton);
+        mStartButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mStartButton.setText(isChecked ? "STOP" : "START");
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Can I help you?", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
 }
